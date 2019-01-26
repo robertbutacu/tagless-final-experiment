b71c3e25c9cc2aab35629d0771b8a3ba1e8cdaa1
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-01-26 20:35:38 +0000

    controller implementation

--- src/main/scala/controllers/BookController.scala
+++ src/main/scala/controllers/BookController.scala
@@ -2,6 +2,7 @@ package controllers
 
 import models.Book
 import models.types.{Author, Year}
+import services.BookService
 
 import scala.language.higherKinds
 
@@ -10,4 +11,24 @@ trait BookController[F[_]] {
   def deleteBook(book: Book): F[Unit]
   def getBookByYearAndAuthor(year: Year, author: Author): F[List[Book]]
   def getAllBooks():          F[List[Book]]
+}
+
+class BookControllerImpl[F[_]](
+                              bookService: BookService[F]
+                              ) extends BookController[F] {
+  override def createBook(book: Book): F[Unit] = {
+    bookService.postBooks(List(book))
+  }
+
+  override def deleteBook(book: Book): F[Unit] = {
+    bookService.deleteBook(book)
+  }
+
+  override def getBookByYearAndAuthor(year: Year, author: Author): F[List[Book]] = {
+    bookService.getBooksByYearAndAuthor(year, author)
+  }
+
+  override def getAllBooks(): F[List[Book]] = {
+    bookService.getAllBooks()
+  }
 }
